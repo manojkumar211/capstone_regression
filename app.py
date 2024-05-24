0a92ea81795003a59c679ba87a7594c7300d70f7
manojkumar211 <mk.chinthakindi@gmail.com>
2024-05-24 13:05:46 +0530

    fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 from sklearn.preprocessing import PolynomialFeatures
 
 
-df_new=pd.DataFrame({'tv_power':[443251.046669,537460.169168],'radio_root':[14405.009866,4302.442699]})
+df_new=pd.DataFrame({'tv_power':[45689.02313546,89765.56967864],'radio_root':[89657.0098456,99978.442699]})
 
 poly=PolynomialFeatures(degree=3)
 data=poly.fit_transform(df_new)
